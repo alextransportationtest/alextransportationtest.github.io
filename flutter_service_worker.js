b28dff114e93a025f54ee84887af165751e200e2
alextransportationtest <80384324+alextransportationtest@users.noreply.github.com>
2023-06-19 20:09:59 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {
 "favicon.png": "107a9e1198486c8ded5da70ef4975ea5",
 "icons/Icon-192.png": "0d069683bd629ee660f47dfba26783ff",
 "icons/Icon-512.png": "107a9e1198486c8ded5da70ef4975ea5",
-"index.html": "7ec4602eb57e29e97bb3f5f809563013",
-"/": "7ec4602eb57e29e97bb3f5f809563013",
-"main.dart.js": "695da8c96f3eea0273e57c583c8bca3c",
+"index.html": "521cc1af90bfd63d4fc04cfd4fd89d89",
+"/": "521cc1af90bfd63d4fc04cfd4fd89d89",
+"main.dart.js": "b1c73484beeed16eee96bdadbb4030f2",
 "manifest.json": "bb8ec6f391223ec0b7e6aa77b2a4f2ec",
 "version.json": "1c6215641e87923334ea8290345eded9"
 };
